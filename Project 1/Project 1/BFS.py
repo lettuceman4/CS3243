703b6529d6421823b26eef156e47363f6a7d09b9
lettuceman4 <65273161+lettuceman4@users.noreply.github.com>
2022-09-03 00:23:10 +0800

    Implement movements of pieces

--- Project 1/Project 1/BFS.py
+++ Project 1/Project 1/BFS.py
@@ -4,27 +4,190 @@ import sys
 #############################################################################
 ######## Piece
 #############################################################################
+def check_if_dest_empty(board, to_pos):
+    # can be obstacles or enemy pieces
+    # if there is obstacle
+    is_obstacle = board.grid[to_pos[0]][to_pos[1]] == -1
+    for e in board.enemy_pieces:
+        if (e == to_pos):
+            return False
+    return is_obstacle
+
+def get_pos_diff(from_pos, to_pos):
+    x = ord(to_pos[0] - from_pos[0])
+    y = to_pos[1] - from_pos[1]
+    return (x, y)
+
+def check_knight(board, from_pos, to_pos):
+    (x, y) = get_pos_diff(from_pos, to_pos)
+    return abs(x * y == 2) & check_if_dest_empty(board, to_pos) 
+
+def check_adj_straight(board, from_pos, to_pos):
+    (x, y) = get_pos_diff(from_pos, to_pos)
+    return x * y == 0 & (x == 1 | y == 1) & check_if_dest_empty(board, to_pos)
+
+def check_straight(board, from_pos, to_pos):
+    is_valid = True
+    (x, y) = get_pos_diff(from_pos, to_pos)
+    is_vertical = x == 0
+    is_horizontal = y == 0 
+    n = x - y
+    step = n/(abs(n))
+
+    # check for every step of the rook, in both directions
+    if (is_vertical):
+        for i in range(0, n, step):
+            prev_coord = (from_pos[0], from_pos[1] + i)
+            next_coord = (from_pos[0], from_pos[1] + i + step)
+            if (not check_adj_straight(board, prev_coord, next_coord)):
+                return False
+    if (is_horizontal):
+        for i in range(0, n. step):
+            prev_coord = (from_pos[0] + i, from_pos[1])
+            next_coord = (from_pos[0] + i + step, from_pos[1])
+            if (not check_adj_straight(board, prev_coord, next_coord)):
+                return False
+    return is_valid
+
+def check_diagonal(board, from_pos, to_pos):
+    is_valid = True
+    (x, y) = get_pos_diff(from_pos, to_pos)[0]
+    n = abs(x)
+    step = x/y 
+
+    print("n: " + n)
+    print("step: " + step)
+
+    for i in range(0, n, step):
+        prev_coord = (from_pos[0] + i, from_pos[1] + i)
+        next_coord = (from_pos[0] + i + step, from_pos[1] + i + step)
+        if (not check_adj_diagonal(board, prev_coord, next_coord)):
+            return False
+    return is_valid
+
+def check_adj_diagonal(board, from_pos, to_pos):
+    (x, y) = get_pos_diff(from_pos, to_pos)
+    return abs(x * y == 1) & check_if_dest_empty(board, to_pos)
+
+def is_straight_line(from_pos, to_pos):
+    (x, y) = get_pos_diff(from_pos, to_pos)
+    return x == 0 | y == 0
+
+def is_diagonal_line(from_pos, to_pos):
+    (x, y) = get_pos_diff(from_pos, to_pos)
+    return abs(x) == abs(y)
+
+    
 class Piece:
-    pass
+    def __init__(self, is_enemy):
+        # standard initialisation of piece
+        self.is_enemy = is_enemy
+
+    def is_valid_move(self, board, from_pos, to_pos):
+        return False
+
+    def __str__(self):
+        return ''
+
+class Rook(Piece):
+    def __init__(self, is_enemy):
+        super(Rook, self).__init__(is_enemy)
+
+    def is_valid_move(self, board, from_pos, to_pos):
+        if (is_straight_line(from_pos, to_pos)):
+            return check_straight(board, from_pos, to_pos)
+        return False
+
+class Knight(Piece):
+    def __init__(self):
+        super().__init__()
+
+    def is_valid_move(board, from_pos, to_pos):
+        return check_knight(board, from_pos, to_pos)
+
+class Bishop(Piece):
+    def __init__(self):
+        super().__init__()
+
+    def is_valid_move(self, board, from_pos, to_pos):
+        if (is_diagonal_line(from_pos, to_pos)):
+            return check_diagonal(board, from_pos, to_pos)
+        return False
+
+class Queen(Piece):
+    def __init__(self):
+        super().__init__()
+
+    def is_valid_move(self, board, from_pos, to_pos):
+        if (is_diagonal_line(from_pos, to_pos)):
+            return check_diagonal(board, from_pos, to_pos)
+        if (is_straight_line(from_pos, to_pos)):
+            return check_straight(board, from_pos, to_pos)
+        return False
+
+class King(Piece):
+    def __init__(self):
+        super().__init__()
+
+    def is_valid_move(self, board, from_pos, to_pos):
+        return check_adj_diagonal(board, from_pos, to_pos) | check_adj_straight(board, from_pos, to_pos)
 
 #############################################################################
 ######## Board
 #############################################################################
-class Board():
-    def __init__(self):
-        pass
+class Board:
+    def __init__(self, rows_no, columns_no, grid, enemy_pieces, own_pieces, goals):
+        self.board = [] * rows_no
+        for i in range(columns_no):
+            self.board.append([None] * columns_no)
+        
+        self.enemy_pieces = enemy_pieces
+        self.grid = grid
+        self.own_pieces = own_pieces
+        self.goal = goals
 
+    def print_board(self):
+        rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        # for i in range(len(rows) - 1):
+        #     print(" " + rows[i] + " ", end=" ")
+        # print(" " + rows[len(rows) - 1] + "  ")
+
+        # buffer = ""
+        # for i in range(41):
+        #     buffer += "*"
+        # print(buffer)
+        # for i in range(len(self.board)):
+        #     tmp_str = "|"
+        #     for j in self.board[i]:
+        #         if j is None or j.name == 'GP':
+        #             tmp_str += "   |"
+        #         elif len(j.name) == 2:
+        #             tmp_str += (" " + str(j) + "|")
+        #         else:
+        #             tmp_str += (" " + str(j) + " |")
+        #     print(tmp_str)
+        # buffer = ""
+        # for i in range(41):
+        #     buffer += "*"
+        # print(buffer)
 #############################################################################
 ######## State
 #############################################################################
 class State:
-    pass
+    def __init__(self):
+        # replace `pass` with the desired attributes and add any
+        # additional parameters to the function
+        pass
+
+    def move(self):
+        pass
 
 #############################################################################
 ######## Implement Search Algorithm
 #############################################################################
 def search(rows, cols, grid, enemy_pieces, own_pieces, goals):
-    pass
+    board = Board(rows_no=rows, columns_no=cols)
+    board.print_board()
 
 
 #############################################################################
@@ -94,4 +257,8 @@ def run_BFS():
     rows, cols, grid, enemy_pieces, own_pieces, goals = parse(testcase)
     moves = search(rows, cols, grid, enemy_pieces, own_pieces, goals)
     return moves
-    
+
+def main():
+    run_BFS()
+if __name__ == "__main__":
+    main()
