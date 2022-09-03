08d449301b665316284eed56640dc69d07c4b3f2
lettuceman4 <65273161+lettuceman4@users.noreply.github.com>
2022-09-04 00:27:51 +0800

    Implement different piece movement rules

--- Project 1/Project 1/BFS.py
+++ Project 1/Project 1/BFS.py
@@ -4,139 +4,109 @@ import sys
 #############################################################################
 ######## Piece
 #############################################################################
-def check_if_dest_empty(board, to_pos):
-    # can be obstacles or enemy pieces
-    # if there is obstacle
-    is_obstacle = board.grid[to_pos[0]][to_pos[1]] == -1
-    for e in board.enemy_pieces:
-        if (e == to_pos):
-            return False
-    return is_obstacle
-
-def get_pos_diff(from_pos, to_pos):
+def get_manhattan_dist(from_pos, to_pos):
     x = ord(to_pos[0] - from_pos[0])
     y = to_pos[1] - from_pos[1]
     return (x, y)
 
-def check_knight(board, from_pos, to_pos):
-    (x, y) = get_pos_diff(from_pos, to_pos)
-    return abs(x * y == 2) & check_if_dest_empty(board, to_pos) 
+def get_knight_moves(board, from_pos):
+    possible_moves = []
+    moves1 = get_moves(board, -2, 1, from_pos, True)
+    moves2 = get_moves(board, -2, -1, from_pos, True)
+    moves3 = get_moves(board, 2, -1, from_pos, True)
+    moves4 = get_moves(board, 2, 1, from_pos, True)
+    moves5 = get_moves(board, 1, 2, from_pos, True)
+    moves6 = get_moves(board, 1, -2, from_pos, True)
+    moves7 = get_moves(board, -1, 2, from_pos, True)
+    moves8 = get_moves(board, -1, -2, from_pos, True)
+    possible_moves.append(moves1)
+    possible_moves.append(moves2)
+    possible_moves.append(moves3)
+    possible_moves.append(moves4)
+    possible_moves.append(moves5)
+    possible_moves.append(moves6)
+    possible_moves.append(moves7)
+    possible_moves.append(moves8)
+    return possible_moves
 
-def check_adj_straight(board, from_pos, to_pos):
-    (x, y) = get_pos_diff(from_pos, to_pos)
-    return x * y == 0 & (x == 1 | y == 1) & check_if_dest_empty(board, to_pos)
+def get_straight_moves(board, curr_pos, is_limited_range):
+    possible_moves = []
+    moves_left = get_moves(board, -1, 0, curr_pos, is_limited_range)
+    moves_right = get_moves(board, 1, 0, curr_pos, is_limited_range)
+    moves_up = get_moves(board, 0, -1, curr_pos, is_limited_range)
+    moves_down = get_moves(board, 0, 1, curr_pos, is_limited_range)
+    possible_moves.append(moves_left)
+    possible_moves.append(moves_right)
+    possible_moves.append(moves_up)
+    possible_moves.append(moves_down)
+    return possible_moves
 
-def check_straight(board, from_pos, to_pos):
-    is_valid = True
-    (x, y) = get_pos_diff(from_pos, to_pos)
-    is_vertical = x == 0
-    is_horizontal = y == 0 
-    n = x - y
-    step = n/(abs(n))
+def get_diagonal_moves(board, from_pos, is_limited_range):
+    possible_moves = []
+    moves1 = get_moves(board, 1, 1, from_pos, is_limited_range)
+    moves2 = get_moves(board, 1, -1, from_pos, is_limited_range)
+    moves3 = get_moves(board, -1, -1, from_pos, is_limited_range)
+    moves4 = get_moves(board, -1, 1, from_pos, is_limited_range)
+    possible_moves.append(moves1)
+    possible_moves.append(moves2)
+    possible_moves.append(moves3)
+    possible_moves.append(moves4)
+    return possible_moves
 
-    # check for every step of the rook, in both directions
-    if (is_vertical):
-        for i in range(0, n, step):
-            prev_coord = (from_pos[0], from_pos[1] + i)
-            next_coord = (from_pos[0], from_pos[1] + i + step)
-            if (not check_adj_straight(board, prev_coord, next_coord)):
-                return False
-    if (is_horizontal):
-        for i in range(0, n. step):
-            prev_coord = (from_pos[0] + i, from_pos[1])
-            next_coord = (from_pos[0] + i + step, from_pos[1])
-            if (not check_adj_straight(board, prev_coord, next_coord)):
-                return False
-    return is_valid
-
-def check_diagonal(board, from_pos, to_pos):
-    is_valid = True
-    (x, y) = get_pos_diff(from_pos, to_pos)[0]
-    n = abs(x)
-    step = x/y 
-
-    print("n: " + n)
-    print("step: " + step)
-
-    for i in range(0, n, step):
-        prev_coord = (from_pos[0] + i, from_pos[1] + i)
-        next_coord = (from_pos[0] + i + step, from_pos[1] + i + step)
-        if (not check_adj_diagonal(board, prev_coord, next_coord)):
-            return False
-    return is_valid
-
-def check_adj_diagonal(board, from_pos, to_pos):
-    (x, y) = get_pos_diff(from_pos, to_pos)
-    return abs(x * y == 1) & check_if_dest_empty(board, to_pos)
-
-def is_straight_line(from_pos, to_pos):
-    (x, y) = get_pos_diff(from_pos, to_pos)
-    return x == 0 | y == 0
-
-def is_diagonal_line(from_pos, to_pos):
-    (x, y) = get_pos_diff(from_pos, to_pos)
-    return abs(x) == abs(y)
+def get_moves(board, step_x, step_y, curr_pos, is_limited_range):
+    moves = []
+    #need to check for threatened position
+    while(curr_pos[0] <= board.columns_no & curr_pos[1] <= board.rows_no):
+        next_pos = (curr_pos[0] + step_x, curr_pos[1] + step_y)
+        if board.grid[next_pos[0]][next_pos[1]] == -1:
+            break
+        else:
+            move_pair = [curr_pos]
+            move_pair.append(next_pos)
+            moves.append(move_pair)
+            curr_pos = next_pos
+            if (is_limited_range):
+                break
+    return moves 
 
     
 class Piece:
-    def __init__(self, is_enemy):
+    def __init__(self, name, from_pos):
         # standard initialisation of piece
-        self.is_enemy = is_enemy
+        self.name = name
+        self.from_pos = from_pos
 
-    def is_valid_move(self, board, from_pos, to_pos):
-        return False
-
-    def __str__(self):
-        return ''
-
-class Rook(Piece):
-    def __init__(self, is_enemy):
-        super(Rook, self).__init__(is_enemy)
-
-    def is_valid_move(self, board, from_pos, to_pos):
-        if (is_straight_line(from_pos, to_pos)):
-            return check_straight(board, from_pos, to_pos)
-        return False
-
-class Knight(Piece):
-    def __init__(self):
-        super().__init__()
-
-    def is_valid_move(board, from_pos, to_pos):
-        return check_knight(board, from_pos, to_pos)
-
-class Bishop(Piece):
-    def __init__(self):
-        super().__init__()
-
-    def is_valid_move(self, board, from_pos, to_pos):
-        if (is_diagonal_line(from_pos, to_pos)):
-            return check_diagonal(board, from_pos, to_pos)
-        return False
-
-class Queen(Piece):
-    def __init__(self):
-        super().__init__()
-
-    def is_valid_move(self, board, from_pos, to_pos):
-        if (is_diagonal_line(from_pos, to_pos)):
-            return check_diagonal(board, from_pos, to_pos)
-        if (is_straight_line(from_pos, to_pos)):
-            return check_straight(board, from_pos, to_pos)
-        return False
-
-class King(Piece):
-    def __init__(self):
-        super().__init__()
-
-    def is_valid_move(self, board, from_pos, to_pos):
-        return check_adj_diagonal(board, from_pos, to_pos) | check_adj_straight(board, from_pos, to_pos)
+    def get_valid_moves(self, board):
+        if (self.name == "Rook"):
+            return get_straight_moves(board, self.from_pos, False)
+        elif (self.name == "Knight"):
+            return get_knight_moves(board, self.from_pos)
+        elif (self.name == "Bishop"):
+            return get_diagonal_moves(board, self.from_pos, False)
+        elif (self.name == "Queen"):
+            moves_straight = get_straight_moves(board, self.from_pos, False)
+            moves_diagonal = get_diagonal_moves(board, self.from_pos, False)
+            return moves_straight.append(moves_diagonal)
+        elif (self.name == "King"):
+            moves_straight = get_straight_moves(board, self.from_pos, True)
+            moves_diagonal = get_diagonal_moves(board, self.from_pos, True)
+            return moves_straight.append(moves_diagonal)
+        elif (self.name == "Ferz"):
+            return get_diagonal_moves(board, self.from_pos, True)
+        elif (self.name == "Princess"):
+            moves_diagonal = get_diagonal_moves(board, self.from_pos, False)
+            moves_knight = get_knight_moves(board, self.from_pos)
+            return moves_diagonal.append(moves_knight)
+        elif (self.name == "Empress"):
+            moves_straight = get_straight_moves(board, self.from_pos, False)
+            moves_knight = get_knight_moves(board, self.from_pos)
+            return moves_straight.append(moves_knight)
 
 #############################################################################
 ######## Board
 #############################################################################
 class Board:
-    def __init__(self, rows_no, columns_no, grid, enemy_pieces, own_pieces, goals):
+    def __init__(self, rows_no, columns_no, grid, enemy_pieces, own_pieces, goals, blocked_grid):
         self.board = [] * rows_no
         for i in range(columns_no):
             self.board.append([None] * columns_no)
@@ -144,32 +114,26 @@ class Board:
         self.enemy_pieces = enemy_pieces
         self.grid = grid
         self.own_pieces = own_pieces
-        self.goal = goals
+        self.goal = goals                    
+        
+        for c in range(columns_no):
+            for r in range(rows_no):
+                if (grid[r][c] == -1):
+                    blocked_grid[r][c] == -1
+        
+        # add enemies possible move
+        enemy_list = []
+        for i in range(len(enemy_pieces)):
+            enemy_list.append(Piece(enemy_pieces[i][0], enemy_pieces[i][1]))
 
-    def print_board(self):
-        rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        # for i in range(len(rows) - 1):
-        #     print(" " + rows[i] + " ", end=" ")
-        # print(" " + rows[len(rows) - 1] + "  ")
-
-        # buffer = ""
-        # for i in range(41):
-        #     buffer += "*"
-        # print(buffer)
-        # for i in range(len(self.board)):
-        #     tmp_str = "|"
-        #     for j in self.board[i]:
-        #         if j is None or j.name == 'GP':
-        #             tmp_str += "   |"
-        #         elif len(j.name) == 2:
-        #             tmp_str += (" " + str(j) + "|")
-        #         else:
-        #             tmp_str += (" " + str(j) + " |")
-        #     print(tmp_str)
-        # buffer = ""
-        # for i in range(41):
-        #     buffer += "*"
-        # print(buffer)
+        blocked_squares = []
+        for i in range(len(enemy_list)):
+            blocked_squares.append(enemy_list[i].get_valid_moves(self.board))
+        
+        for s in blocked_squares:
+            (r, c) = s[0]
+            blocked_grid[r][c] = -1
+        
 #############################################################################
 ######## State
 #############################################################################
