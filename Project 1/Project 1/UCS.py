ed53e15d6fc475696151e2b8d11eb4d200cdbf09
lettuceman4 <65273161+lettuceman4@users.noreply.github.com>
2022-09-09 16:02:25 +0800

    Implement UCS

--- Project 1/Project 1/UCS.py
+++ Project 1/Project 1/UCS.py
@@ -1,30 +1,256 @@
+from queue import LifoQueue, PriorityQueue
 import sys
 
 # Helper functions to aid in your implementation. Can edit/remove
 #############################################################################
 ######## Piece
 #############################################################################
+def get_manhattan_dist(from_pos, to_pos):
+    x = ord(to_pos[0] - from_pos[0])
+    y = to_pos[1] - from_pos[1]
+    return (x, y)
+
+def get_knight_moves(grid, from_pos):
+    possible_moves = []
+    curr_pos_chess_coord = to_chess_coord(from_pos)
+    possible_moves.append(curr_pos_chess_coord)
+    moves1 = get_moves(grid, -2, 1, from_pos, True)
+    moves2 = get_moves(grid, -2, -1, from_pos, True)
+    moves3 = get_moves(grid, 2, -1, from_pos, True)
+    moves4 = get_moves(grid, 2, 1, from_pos, True)
+    moves5 = get_moves(grid, 1, 2, from_pos, True)
+    moves6 = get_moves(grid, 1, -2, from_pos, True)
+    moves7 = get_moves(grid, -1, 2, from_pos, True)
+    moves8 = get_moves(grid, -1, -2, from_pos, True)
+    possible_moves.append(moves1)
+    possible_moves.append(moves2)
+    possible_moves.append(moves3)
+    possible_moves.append(moves4)
+    possible_moves.append(moves5)
+    possible_moves.append(moves6)
+    possible_moves.append(moves7)
+    possible_moves.append(moves8)
+    return list(filter(lambda x: x, possible_moves))
+
+def get_straight_moves(grid, curr_pos, is_limited_range):
+    possible_moves = []
+    curr_pos_chess_coord = to_chess_coord(curr_pos)
+    possible_moves.append(curr_pos_chess_coord)
+    moves_left = get_moves(grid, -1, 0, curr_pos, is_limited_range)
+    moves_right = get_moves(grid, 1, 0, curr_pos, is_limited_range)
+    moves_up = get_moves(grid, 0, -1, curr_pos, is_limited_range)
+    moves_down = get_moves(grid, 0, 1, curr_pos, is_limited_range)
+    possible_moves.append(moves_left)
+    possible_moves.append(moves_right)
+    possible_moves.append(moves_up)
+    possible_moves.append(moves_down)
+    return list(filter(lambda x: x, possible_moves))
+
+def get_diagonal_moves(grid, from_pos, is_limited_range):
+    possible_moves = []
+    curr_pos_chess_coord = to_chess_coord(from_pos)
+    possible_moves.append(curr_pos_chess_coord)
+    moves1 = get_moves(grid, 1, 1, from_pos, is_limited_range)
+    moves2 = get_moves(grid, 1, -1, from_pos, is_limited_range)
+    moves3 = get_moves(grid, -1, -1, from_pos, is_limited_range)
+    moves4 = get_moves(grid, -1, 1, from_pos, is_limited_range)
+    possible_moves.append(moves1)
+    possible_moves.append(moves2)
+    possible_moves.append(moves3)
+    possible_moves.append(moves4)
+    return list(filter(lambda x: x, possible_moves))
+
+def is_still_in_board(grid, grid_pos):
+    return grid_pos[0] < len(grid) and grid_pos[1] < len(grid[0]) and grid_pos[0] >= 0 and grid_pos[1] >= 0 
+
+def get_moves(grid, step_x, step_y, curr_pos, is_limited_range):
+    moves = []
+    #need to check for threatened position
+    # need to check if its out of the board
+    while(is_still_in_board(grid, curr_pos)):
+        next_pos = (curr_pos[0] + step_x, curr_pos[1] + step_y)
+        if (not is_still_in_board(grid, next_pos)):
+            break
+        if grid[next_pos[0]][next_pos[1]] < 0:
+            break
+        else:
+            next_pos_chess_coord = to_chess_coord(next_pos)
+            moves.append(next_pos_chess_coord)
+            curr_pos = next_pos
+            if (is_limited_range):            
+                break
+    # print ("step: ({}, {}): {}".format(step_x, step_y, moves))
+    return moves 
+
+    
 class Piece:
-    pass
+    def __init__(self, name, from_pos):
+        # standard initialisation of piece
+        self.name = name
+        self.from_pos = from_pos
+
+    def get_valid_moves(self, grid):
+        if (self.name == "Rook"):
+            return get_straight_moves(grid, self.from_pos, False)
+        elif (self.name == "Knight"):
+            return get_knight_moves(grid, self.from_pos)
+        elif (self.name == "Bishop"):
+            return get_diagonal_moves(grid, self.from_pos, False)
+        elif (self.name == "Queen"):
+            moves_straight = get_straight_moves(grid, self.from_pos, False)
+            moves_diagonal = get_diagonal_moves(grid, self.from_pos, False)
+            moves_straight.append(moves_diagonal)
+            return moves_straight
+        elif (self.name == "King"):
+            moves_straight = get_straight_moves(grid, self.from_pos, True)
+            moves_diagonal = get_diagonal_moves(grid, self.from_pos, True)
+            moves_straight.append(moves_diagonal)
+            return moves_straight
+        elif (self.name == "Ferz"):
+            return get_diagonal_moves(grid, self.from_pos, True)
+        elif (self.name == "Princess"):
+            moves_diagonal = get_diagonal_moves(grid, self.from_pos, False)
+            moves_knight = get_knight_moves(grid, self.from_pos)
+            moves_diagonal.append(moves_knight)
+            return moves_diagonal
+        elif (self.name == "Empress"):
+            moves_straight = get_straight_moves(grid, self.from_pos, False)
+            moves_knight = get_knight_moves(grid, self.from_pos)
+            moves_straight.append(moves_knight)
+            return moves_straight
+
+def flatten(S):
+    if S == []:
+        return S
+    if isinstance(S[0], list):
+        return flatten(S[0]) + flatten(S[1:])
+    return S[:1] + flatten(S[1:])
 
 #############################################################################
 ######## Board
 #############################################################################
+def to_chess_coord(grid_pos):
+    return (chr(grid_pos[1] + 97), grid_pos[0])
+
+def to_grid_coord(chess_pos):
+    return (chess_pos[1], ord(chess_pos[0]) - 97)
+
 class Board:
-    pass
+    def __init__(self, rows_no, columns_no, grid, enemy_pieces, own_pieces, goals): 
+        self.board = [] * rows_no
+        for i in range(columns_no):
+            self.board.append([None] * columns_no)
+        
+        self.enemy_pieces = enemy_pieces
+        self.grid = grid
+        self.own_pieces = own_pieces
+        self.goal = goals
+
+        # add enemies possible move
+        enemy_list = []
+        
+        for i in range(len(enemy_pieces)):
+            enemy_list.append(Piece(enemy_pieces[i][0], enemy_pieces[i][1]))
+
+        possible_enemy_moves = []
+        for i in range(len(enemy_list)):
+            new_list = enemy_list[i].get_valid_moves(self.grid)
+            possible_enemy_moves.append(new_list)
+    
+        possible_enemy_moves = flatten(possible_enemy_moves)
+        blocked_square = set()
+        for i in range (len(possible_enemy_moves)):
+            curr_square = possible_enemy_moves[i]
+            blocked_square.add(curr_square)
+            (r, c) = to_grid_coord(curr_square)
+            grid[r][c] = -2
 
 #############################################################################
 ######## State
 #############################################################################
-class State:
-    pass
+# class State:
+#     def __init__(self, board):
+#         self.board = board
+        
+#         # construct search space
+        
+    
+#     def move(self):
+#         pass
+         
 
 #############################################################################
 ######## Implement Search Algorithm
 #############################################################################
-def search(rows, cols, grid, enemy_pieces, own_pieces, goals):
-    pass
+def goal_test(goal_lst, curr_pos):
+    for i in range(len(goal_lst)):
+        if curr_pos == goal_lst[i]:
+            return (True, goal_lst[i])
+    return (False, None)
 
+def search(rows, cols, grid, enemy_pieces, own_pieces, goals):
+    board = Board(rows, cols, grid, enemy_pieces, own_pieces, goals)
+    # state = State(board)
+    # for each movement of the king, 
+    # find possible move, then
+    # add them all to the frontier. 
+    # pop the stack. 
+    # add to path 
+    frontier = PriorityQueue()
+    path = []
+    path_found = False
+    first_goal_found = None
+    parent = dict()
+    visited = set()
+    curr_cost = 0
+    
+    # for i in range(len(own_pieces)):
+    for i in range(len(own_pieces)):
+        piece = Piece(own_pieces[i][0], own_pieces[i][1])
+        frontier.put((0, piece.from_pos))
+        parent[piece.from_pos] = None
+        while not frontier.empty():
+            curr_node = frontier.get()
+            curr_cost = curr_node[0]
+            if (curr_node[1] not in visited):
+                visited.add(curr_node[1])
+
+            # curr_node[0]: cost
+            # curr_node[1]: grid coord position
+            if goal_test(goals, curr_node[1])[0]:
+                path_found = True
+                first_goal_found = goal_test(goals, curr_node[1])[1]
+                break
+
+            piece = Piece(own_pieces[i][0], curr_node[1])
+            possible_moves = piece.get_valid_moves(board.grid)
+            possible_moves = flatten(possible_moves)
+            possible_moves = list(dict.fromkeys(possible_moves))
+            
+            # add neighbour to frontier
+            for j in range(len(possible_moves)):
+                neighbour = to_grid_coord(possible_moves[j])
+                if (neighbour != curr_node[1] and neighbour not in visited):
+                    visited.add(neighbour)
+                    parent[neighbour] = curr_node[1]
+                    (r, c) = neighbour
+                    curr_cost = curr_node[0] + grid[r][c]
+                    frontier.put((curr_cost, neighbour))
+    # backtrack
+    if path_found:
+        # path.append(first_goal_found)
+        while parent[first_goal_found] is not None:
+            first_goal_found_chess_coord = to_chess_coord(first_goal_found)
+            move_pair = [first_goal_found_chess_coord]
+            next_step = parent[first_goal_found]
+            next_step_chess_coord = to_chess_coord(next_step)
+            move_pair.append(next_step_chess_coord)
+            move_pair.reverse()
+            path.append(move_pair)
+            first_goal_found = parent[first_goal_found]
+        path.reverse()
+    # print((path, curr_cost))
+    return path, curr_cost
 
 #############################################################################
 ######## Parser function and helper functions
@@ -89,3 +315,9 @@ def run_UCS():
     rows, cols, grid, enemy_pieces, own_pieces, goals = parse(testcase)
     moves, pathcost = search(rows, cols, grid, enemy_pieces, own_pieces, goals)
     return moves, pathcost
+
+def main():
+    run_UCS()
+
+if __name__ == "__main__":
+    main()
