0c55de286896cbe282ed32546d9d10b6586b4c7e
lettuceman4 <65273161+lettuceman4@users.noreply.github.com>
2022-10-24 21:19:53 +0800

    Handle killing opponent pieces

--- Project 3/Project 3/AB.py
+++ Project 3/Project 3/AB.py
@@ -1,24 +1,30 @@
 import sys
+from tkinter.tix import COLUMN
 
 ### IMPORTANT: Remove any print() functions or rename any print functions/variables/string when submitting on CodePost
 ### The autograder will not run if it detects any print function.
+
+WHITE = "White"
+BLACK = "Black"
+ROWS = 7
+COLUMNS = 7
 
 # Helper functions to aid in your implementation. Can edit/remove
 #############################################################################
 ######## Piece
 #############################################################################
-def get_knight_moves(grid, from_pos):
+def get_knight_moves(grid, from_pos, is_white):
     possible_moves = []
     # curr_pos_chess_coord = to_chess_coord(from_pos)
     possible_moves.append(from_pos)
-    moves1 = get_moves(grid, -2, 1, from_pos, True)
-    moves2 = get_moves(grid, -2, -1, from_pos, True)
-    moves3 = get_moves(grid, 2, -1, from_pos, True)
-    moves4 = get_moves(grid, 2, 1, from_pos, True)
-    moves5 = get_moves(grid, 1, 2, from_pos, True)
-    moves6 = get_moves(grid, 1, -2, from_pos, True)
-    moves7 = get_moves(grid, -1, 2, from_pos, True)
-    moves8 = get_moves(grid, -1, -2, from_pos, True)
+    moves1 = get_moves(grid, -2, 1, from_pos, True, is_white)
+    moves2 = get_moves(grid, -2, -1, from_pos, True, is_white)
+    moves3 = get_moves(grid, 2, -1, from_pos, True, is_white)
+    moves4 = get_moves(grid, 2, 1, from_pos, True, is_white)
+    moves5 = get_moves(grid, 1, 2, from_pos, True, is_white)
+    moves6 = get_moves(grid, 1, -2, from_pos, True, is_white)
+    moves7 = get_moves(grid, -1, 2, from_pos, True, is_white)
+    moves8 = get_moves(grid, -1, -2, from_pos, True, is_white)
     possible_moves.append(moves1)
     possible_moves.append(moves2)
     possible_moves.append(moves3)
@@ -29,28 +35,28 @@ def get_knight_moves(grid, from_pos):
     possible_moves.append(moves8)
     return list(filter(lambda x: x, possible_moves))
 
-def get_straight_moves(grid, curr_pos, is_limited_range):
+def get_straight_moves(grid, curr_pos, is_limited_range, is_white):
     possible_moves = []
     # curr_pos_chess_coord = to_chess_coord(curr_pos)
     possible_moves.append(curr_pos)
-    moves_left = get_moves(grid, -1, 0, curr_pos, is_limited_range)
-    moves_right = get_moves(grid, 1, 0, curr_pos, is_limited_range)
-    moves_up = get_moves(grid, 0, -1, curr_pos, is_limited_range)
-    moves_down = get_moves(grid, 0, 1, curr_pos, is_limited_range)
+    moves_left = get_moves(grid, -1, 0, curr_pos, is_limited_range, is_white)
+    moves_right = get_moves(grid, 1, 0, curr_pos, is_limited_range, is_white)
+    moves_up = get_moves(grid, 0, -1, curr_pos, is_limited_range, is_white)
+    moves_down = get_moves(grid, 0, 1, curr_pos, is_limited_range, is_white)
     possible_moves.append(moves_left)
     possible_moves.append(moves_right)
     possible_moves.append(moves_up)
     possible_moves.append(moves_down)
     return list(filter(lambda x: x, possible_moves))
 
-def get_diagonal_moves(grid, from_pos, is_limited_range):
+def get_diagonal_moves(grid, from_pos, is_limited_range, is_white):
     possible_moves = []
     # curr_pos_chess_coord = to_chess_coord(from_pos)
     possible_moves.append(from_pos)
-    moves1 = get_moves(grid, 1, 1, from_pos, is_limited_range)
-    moves2 = get_moves(grid, 1, -1, from_pos, is_limited_range)
-    moves3 = get_moves(grid, -1, -1, from_pos, is_limited_range)
-    moves4 = get_moves(grid, -1, 1, from_pos, is_limited_range)
+    moves1 = get_moves(grid, 1, 1, from_pos, is_limited_range, is_white)
+    moves2 = get_moves(grid, 1, -1, from_pos, is_limited_range, is_white)
+    moves3 = get_moves(grid, -1, -1, from_pos, is_limited_range, is_white)
+    moves4 = get_moves(grid, -1, 1, from_pos, is_limited_range, is_white)
     possible_moves.append(moves1)
     possible_moves.append(moves2)
     possible_moves.append(moves3)
@@ -60,7 +66,7 @@ def get_diagonal_moves(grid, from_pos, is_limited_range):
 def is_still_in_board(grid, grid_pos):
     return grid_pos[0] < len(grid) and grid_pos[1] < len(grid[0]) and grid_pos[0] >= 0 and grid_pos[1] >= 0 
 
-def get_moves(grid, step_x, step_y, curr_pos, is_limited_range):
+def get_moves(grid, step_x, step_y, curr_pos, is_limited_range, is_white):
     moves = []
     #need to check for threatened position
     # need to check if its out of the board
@@ -68,54 +74,60 @@ def get_moves(grid, step_x, step_y, curr_pos, is_limited_range):
         next_pos = (curr_pos[0] + step_x, curr_pos[1] + step_y)
         if (not is_still_in_board(grid, next_pos)):
             break
-        # if the position is blocked
-        if grid[next_pos[0]][next_pos[1]] < 0:
-            break
-        else:
-            # next_pos_chess_coord = to_chess_coord(next_pos)
+        if (include_next_pos(next_pos=next_pos, is_white=is_white, grid=grid)):
             moves.append(next_pos)
             curr_pos = next_pos
             if (is_limited_range):            
                 break
+        else:
+            break
     return moves 
 
-    
+def include_next_pos(next_pos, is_white: bool, grid):
+    if is_white:
+        # blocked by its own piece
+        return not grid[next_pos[0]][next_pos[1]].is_white
+    else:
+        return grid[next_pos[0]][next_pos[1]].is_white
+
+
 class Piece:
-    def __init__(self, name, from_pos):
+    def __init__(self, name, from_pos, is_white):
         # standard initialisation of piece
         self.name = name
         self.from_pos = from_pos
+        self.is_white = is_white
     
     def update_from_pos(self, from_pos):
         self.from_pos = from_pos
         
     def get_valid_moves(self, grid):
         if (self.name == "Rook"):
-            return flatten(get_straight_moves(grid, self.from_pos, False))
+            return flatten(get_straight_moves(grid, self.from_pos, False, self.is_white))
         elif (self.name == "Knight"):
-            return flatten(get_knight_moves(grid, self.from_pos))
+            return flatten(get_knight_moves(grid, self.from_pos, self.is_white))
         elif (self.name == "Bishop"):
-            return flatten(get_diagonal_moves(grid, self.from_pos, False))
+            return flatten(get_diagonal_moves(grid, self.from_pos, False, self.is_white))
         elif (self.name == "Queen"):
-            moves_straight = get_straight_moves(grid, self.from_pos, False)
-            moves_diagonal = get_diagonal_moves(grid, self.from_pos, False)
+            moves_straight = get_straight_moves(grid, self.from_pos, False, self.is_white)
+            moves_diagonal = get_diagonal_moves(grid, self.from_pos, False, self.is_white)
             moves_straight.append(moves_diagonal)
             return flatten(moves_straight)
         elif (self.name == "King"):
-            moves_straight = get_straight_moves(grid, self.from_pos, True)
-            moves_diagonal = get_diagonal_moves(grid, self.from_pos, True)
+            moves_straight = get_straight_moves(grid, self.from_pos, True, self.is_white)
+            moves_diagonal = get_diagonal_moves(grid, self.from_pos, True, self.is_white)
             moves_straight.append(moves_diagonal)
             return flatten(moves_straight)
         elif (self.name == "Ferz"):
-            return flatten(get_diagonal_moves(grid, self.from_pos, True))
+            return flatten(get_diagonal_moves(grid, self.from_pos, True, self.is_white))
         elif (self.name == "Princess"):
-            moves_diagonal = get_diagonal_moves(grid, self.from_pos, False)
-            moves_knight = get_knight_moves(grid, self.from_pos)
+            moves_diagonal = get_diagonal_moves(grid, self.from_pos, False, self.is_white)
+            moves_knight = get_knight_moves(grid, self.from_pos, self.is_white)
             moves_diagonal.append(moves_knight)
             return flatten(moves_diagonal)
         elif (self.name == "Empress"):
-            moves_straight = get_straight_moves(grid, self.from_pos, False)
-            moves_knight = get_knight_moves(grid, self.from_pos)
+            moves_straight = get_straight_moves(grid, self.from_pos, False, self.is_white)
+            moves_knight = get_knight_moves(grid, self.from_pos, self.is_white)
             moves_straight.append(moves_knight)
             return flatten(moves_straight)
 
@@ -130,18 +142,19 @@ def flatten(S):
 ######## Board
 #############################################################################
 def to_chess_coord(grid_pos):
-    return (chr(grid_pos[1] + 97), grid_pos[0] + 1)
+    return (chr(grid_pos[1] + 97), grid_pos[0])
 
 def to_grid_coord(chess_pos):
     return (chess_pos[1], ord(chess_pos[0]) - 97)
 
 class Board:
-    def __init__(self, rows_no, columns_no, grid, given_pieces_num): 
-        self.board = [] * rows_no
-        for i in range(columns_no):
-            self.board.append([None] * columns_no)
-        
+    def __init__(self, grid, given_pieces_num): 
         self.grid = grid
+
+# gameboard: Dictionary of positions (Key) to the tuple of piece type and its colour (Value). This represents the current pieces left on the board.
+# Key: position is a tuple with the x-axis in String format and the y-axis in integer format.
+# Value: tuple of piece type and piece colour with both values being in String format. Note that the first letter for both type and colour are capitalized as well.
+# gameboard example: {('a', 0) : ('Queen', 'White'), ('d', 10) : ('Knight', 'Black'), ('g', 25) : ('Rook', 'White')}
 
 #Implement your minimax with alpha-beta pruning algorithm here.
 def ab(gameboard):
@@ -196,6 +209,7 @@ def from_chess_coord( ch_coord):
 def setUpBoard():
     config = sys.argv[1]
     rows, cols, gameboard = parse(config)
+    return rows, cols, gameboard
 
 ### DO NOT EDIT/REMOVE THE FUNCTION HEADER BELOW###
 # Chess Pieces: King, Queen, Knight, Bishop, Rook, Princess, Empress, Ferz, Pawn (First letter capitalized)
@@ -214,6 +228,22 @@ def setUpBoard():
 
 def studentAgent(gameboard):
     # You can code in here but you cannot remove this function, change its parameter or change the return type
+    grid = [[None for _ in range(COLUMNS)] for _ in range(ROWS)]
+    for pos, piece in gameboard.items():
+        grid_pos = to_grid_coord(pos)
+        piece_obj = Piece(piece[0], grid_pos, piece[1] == WHITE)
+        grid[grid_pos[0]][grid_pos[1]] = piece_obj
+    
+    board = Board(grid)
     move = ab(gameboard)
     return move #Format to be returned (('a', 0), ('b', 3))
 
+def main():
+    print(to_grid_coord(('a', 0)))
+    print(to_chess_coord((0, 0)))
+    board = setUpBoard()[2]
+    print(studentAgent(board))
+
+
+if __name__ == "__main__":
+    main()
