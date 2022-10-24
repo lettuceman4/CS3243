67570476726d09925972bdd4af0dfeafafc67dd5
lettuceman4 <65273161+lettuceman4@users.noreply.github.com>
2022-10-24 23:16:33 +0800

    Add pawn move

--- Project 3/Project 3/AB.py
+++ Project 3/Project 3/AB.py
@@ -1,5 +1,4 @@
 import sys
-from tkinter.tix import COLUMN
 
 ### IMPORTANT: Remove any print() functions or rename any print functions/variables/string when submitting on CodePost
 ### The autograder will not run if it detects any print function.
@@ -8,11 +7,46 @@ WHITE = "White"
 BLACK = "Black"
 ROWS = 7
 COLUMNS = 7
+DEPTH = 4
+PIECE_VALUES = {
+    "Pawn": 1,
+    "Ferz": 2,
+    "Knight": 3,
+    "Bishop": 4,
+    "Rook": 5,
+    "Princess": 6,
+    "Empress": 7,
+    "Queen": 8,
+     "King": 100,
+}
 
 # Helper functions to aid in your implementation. Can edit/remove
 #############################################################################
 ######## Piece
 #############################################################################
+def is_still_in_board(grid, grid_pos):
+    return grid_pos[0] < len(grid) and grid_pos[1] < len(grid[0]) and grid_pos[0] >= 0 and grid_pos[1] >= 0 
+
+def get_moves(grid, step_x, step_y, curr_pos, is_limited_range, is_white):
+    moves = []
+    #need to check for threatened position
+    # need to check if its out of the board
+    while(is_still_in_board(grid, curr_pos)):
+        next_pos = (curr_pos[0] + step_x, curr_pos[1] + step_y)
+        if (not is_still_in_board(grid, next_pos)):
+            break
+        if (include_next_pos(next_pos=next_pos, is_white=is_white, grid=grid)):
+            moves.append(next_pos)
+            curr_pos = next_pos
+            if (is_limited_range):            
+                break
+        else:
+            break
+    return moves 
+
+def include_next_pos(next_pos, is_white: bool, grid):
+    return grid[next_pos[0]][next_pos[1]].is_white != is_white
+
 def get_knight_moves(grid, from_pos, is_white):
     possible_moves = []
     # curr_pos_chess_coord = to_chess_coord(from_pos)
@@ -34,6 +68,32 @@ def get_knight_moves(grid, from_pos, is_white):
     possible_moves.append(moves7)
     possible_moves.append(moves8)
     return list(filter(lambda x: x, possible_moves))
+
+def is_pawn_move_included(diag_pos, grid, is_white):
+    if (is_still_in_board(grid, diag_pos)):
+        if (grid[diag_pos[0]][diag_pos[1]] is not None):
+            piece = grid[diag_pos[0]][diag_pos[1]]
+            return piece.is_white != is_white
+    return False
+
+def get_pawn_moves(grid, curr_pos, is_white):
+    moves = []
+    dir = 1 if is_white else -1
+    next_pos_straight = (curr_pos[0], curr_pos[1] + dir)
+    next_pos_diagonal_right = (curr_pos[0] + dir, curr_pos[1] + dir)
+    next_pos_diagonal_left = (curr_pos[0] - dir, curr_pos[1] + dir)
+    
+    # straight move only execute if no piece in front
+    if (is_still_in_board(grid, next_pos_straight) 
+    and grid[next_pos_straight[0]][next_pos_straight[1]] is None):
+        moves.append(next_pos_straight)
+
+    if (is_pawn_move_included(next_pos_diagonal_right)):
+        moves.append(next_pos_diagonal_right)
+    
+    if (is_pawn_move_included(next_pos_diagonal_left)):
+        moves.append(next_pos_diagonal_left)
+    return moves
 
 def get_straight_moves(grid, curr_pos, is_limited_range, is_white):
     possible_moves = []
@@ -63,34 +123,6 @@ def get_diagonal_moves(grid, from_pos, is_limited_range, is_white):
     possible_moves.append(moves4)
     return list(filter(lambda x: x, possible_moves))
 
-def is_still_in_board(grid, grid_pos):
-    return grid_pos[0] < len(grid) and grid_pos[1] < len(grid[0]) and grid_pos[0] >= 0 and grid_pos[1] >= 0 
-
-def get_moves(grid, step_x, step_y, curr_pos, is_limited_range, is_white):
-    moves = []
-    #need to check for threatened position
-    # need to check if its out of the board
-    while(is_still_in_board(grid, curr_pos)):
-        next_pos = (curr_pos[0] + step_x, curr_pos[1] + step_y)
-        if (not is_still_in_board(grid, next_pos)):
-            break
-        if (include_next_pos(next_pos=next_pos, is_white=is_white, grid=grid)):
-            moves.append(next_pos)
-            curr_pos = next_pos
-            if (is_limited_range):            
-                break
-        else:
-            break
-    return moves 
-
-def include_next_pos(next_pos, is_white: bool, grid):
-    if is_white:
-        # blocked by its own piece
-        return not grid[next_pos[0]][next_pos[1]].is_white
-    else:
-        return grid[next_pos[0]][next_pos[1]].is_white
-
-
 class Piece:
     def __init__(self, name, from_pos, is_white):
         # standard initialisation of piece
@@ -104,6 +136,8 @@ class Piece:
     def get_valid_moves(self, grid):
         if (self.name == "Rook"):
             return flatten(get_straight_moves(grid, self.from_pos, False, self.is_white))
+        elif (self.name == "Pawn"):
+            return flatten(get_straight_moves(grid, self.pos, ))
         elif (self.name == "Knight"):
             return flatten(get_knight_moves(grid, self.from_pos, self.is_white))
         elif (self.name == "Bishop"):
@@ -147,17 +181,32 @@ def to_chess_coord(grid_pos):
 def to_grid_coord(chess_pos):
     return (chess_pos[1], ord(chess_pos[0]) - 97)
 
+# should only change when a move is made
 class Board:
-    def __init__(self, grid, given_pieces_num): 
+    def __init__(self, grid): 
         self.grid = grid
 
-# gameboard: Dictionary of positions (Key) to the tuple of piece type and its colour (Value). This represents the current pieces left on the board.
-# Key: position is a tuple with the x-axis in String format and the y-axis in integer format.
-# Value: tuple of piece type and piece colour with both values being in String format. Note that the first letter for both type and colour are capitalized as well.
-# gameboard example: {('a', 0) : ('Queen', 'White'), ('d', 10) : ('Knight', 'Black'), ('g', 25) : ('Rook', 'White')}
+# is dynamic
+class State:
+    def __init__(self, grid): 
+        self.grid = grid
+    
+    # white tries to maximise this, black tries to minimise
+    def get_objective_value_of_state(self):
+        white_value = 0
+        black_value = 0
+        for j in range(COLUMNS):
+            for i in range(ROWS):
+                if (self.grid[i][j] is not None):
+                    piece = self.grid[i][j]
+                    if (piece.is_white):
+                        white_value += PIECE_VALUES[piece.name]
+                    else:
+                        black_value += PIECE_VALUES[piece.name]
+        return white_value - black_value
 
 #Implement your minimax with alpha-beta pruning algorithm here.
-def ab(gameboard):
+def ab(board):
     pass
 
 #############################################################################
@@ -230,17 +279,14 @@ def studentAgent(gameboard):
     # You can code in here but you cannot remove this function, change its parameter or change the return type
     grid = [[None for _ in range(COLUMNS)] for _ in range(ROWS)]
     for pos, piece in gameboard.items():
-        grid_pos = to_grid_coord(pos)
-        piece_obj = Piece(piece[0], grid_pos, piece[1] == WHITE)
-        grid[grid_pos[0]][grid_pos[1]] = piece_obj
+        piece_obj = Piece(piece[0], pos, piece[1] == WHITE)
+        grid[pos[0]][pos[1]] = piece_obj
     
     board = Board(grid)
     move = ab(gameboard)
     return move #Format to be returned (('a', 0), ('b', 3))
 
 def main():
-    print(to_grid_coord(('a', 0)))
-    print(to_chess_coord((0, 0)))
     board = setUpBoard()[2]
     print(studentAgent(board))
 
